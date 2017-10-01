2eeb2eedd212fc361249c58f4d36dd5a7bab0d1b
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-10-01 01:25:06 -0600

    Example plugin installed dashboard message

--- rar-example-plugin.php
+++ rar-example-plugin.php
@@ -9,3 +9,12 @@ Author URI: http://rafalfaro.com
 Licnse: MIT
 License URI: https://github.com/rafalfaro18/rar-example-plugin/blob/master/LICENSE
 */
+
+function sample_admin_notice__success() {
+    ?>
+    <div class="notice notice-success is-dismissible">
+        <p><?php _e( 'RAR Example Plugin Installed!', 'sample-text-domain' ); ?></p>
+    </div>
+    <?php
+}
+add_action( 'admin_notices', 'sample_admin_notice__success' );
