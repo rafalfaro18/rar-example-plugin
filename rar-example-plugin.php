dbbc0aaf6d5a0936caefd829829a19d7bcd0dd8e
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-10-01 01:05:28 -0600

    Added Version

--- rar-example-plugin.php
+++ rar-example-plugin.php
@@ -2,5 +2,6 @@
 /*
 Plugin Name: RAR Example Plugin
 Plugin URI: https://github.com/rafalfaro18/rar-example-plugin
-Description: Wordpress example Plugin for testing purposes
+Description: Wordpress example Plugin for testing purposesg
+Version: 1.0
 */
