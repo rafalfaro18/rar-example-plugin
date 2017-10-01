9d6a73d16ac5859d6f7e2cfda1c612458457b55b
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-10-01 01:16:27 -0600

    License

--- rar-example-plugin.php
+++ rar-example-plugin.php
@@ -6,4 +6,6 @@ Description: Wordpress example Plugin for testing purposesg
 Version: 1.0
 Author: Rafael Alfaro
 Author URI: http://rafalfaro.com
+Licnse: MIT
+License URI: https://github.com/rafalfaro18/rar-example-plugin/blob/master/LICENSE
 */
