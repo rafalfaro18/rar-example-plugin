9eaca7d654487bd56634d56cd610ddfb9def9ab5
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-10-01 01:07:13 -0600

    Added author and author uri

--- rar-example-plugin.php
+++ rar-example-plugin.php
@@ -4,4 +4,6 @@ Plugin Name: RAR Example Plugin
 Plugin URI: https://github.com/rafalfaro18/rar-example-plugin
 Description: Wordpress example Plugin for testing purposesg
 Version: 1.0
+Author: Rafael Alfaro
+Author URI: http://rafalfaro.com
 */
